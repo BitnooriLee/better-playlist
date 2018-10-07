9130e35c8bc087016fcd466237e06102742f9ea1
Bitnoori Lee <bitnoori.lee@gmail.com>
2018-10-07 13:26:56 +0200

    redirect url

--- src/App.js
+++ src/App.js
@@ -80,8 +80,7 @@ class App extends Component {
     let parsed = queryString.parse(window.location.search);
     let accessToken = parsed.access_token;
     if(!accessToken)
-      return; 
-
+      return;
     fetch ('https://api.spotify.com/v1/me',{
       headers: {'Authorization': 'Bearer ' + accessToken}
     }).then(response => response.json())
@@ -128,7 +127,11 @@ class App extends Component {
         {playlistsToRender.map(playlist =>
           <Playlist playlist={playlist} />
         )}
-      </div> : <button onClick={() => window.location='http://localhost:8888/login'}
+      </div> : <button onClick={() => {
+     window.location = window.location.includes('localhost')
+        ? 'http://localhost:8888/login'
+        : 'https://better-playlists-bit.herokuapp.com/login'}
+      }
       style={{padding: '20px', 'font-size': '50px', 'margin-top':'20px'}}>Sign in with Spotify</button>
       }
     </div>
